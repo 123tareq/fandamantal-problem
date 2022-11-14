43b87c32d394d321df01fb05b032914bbd9b6fd6
123tareq <tarakhassan508@gmail.com>
2022-11-14 11:44:40 +0600

    pactece problem

---
+++ index.js
@@ -0,0 +1,60 @@
+var num1=12;
+var num2=13;
+var totalAdiiton=num1+num2;
+console.log(totalAdiiton);
+
+var num3=10;
+var num4=5;
+var substruction=num3-num4;
+console.log(substruction);
+
+var num5=10;
+var num6=5;
+var multiplication= num5*num6;
+console.log(multiplication);
+
+var diviton1=25;
+var diviton2=3;
+var divitonResult=diviton1/diviton2;
+console.log(divitonResult);
+
+
+var modulas1=12;
+var modulas2= 5;
+var modulasResult=modulas1%modulas2;
+console.log(modulasResult);
+
+// string to number convert lotrseint mathod
+
+var stringKyword='12';
+var stringToNum= parseInt(stringKyword);
+console.log(typeof(stringToNum));
+
+// number to string convert flotint mathod
+
+var stingConvert=15;
+var text=stingConvert.toString();
+console.log(typeof(text));
+
+// flot to int convert
+var flotNum=45.555;
+console.log(parseInt(flotNum));
+
+// fixed flot mathod
+
+var flotNumber=34.499999999999999;
+var fixedFlotNumber= flotNumber.toFixed(3);
+console.log(flotNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task.js
@@ -0,0 +1,25 @@
+// practice problem 1
+var totalMony=1000;
+var orangesCost=700;
+var shopkeeperRetun=totalMony-orangesCost;
+console.log(shopkeeperRetun);
+
+// practice problem 2
+
+var allSubjectNumber= 75.25+65+80+35.45+99.50;
+var average=allSubjectNumber/5;
+var fixdnumber= average.toFixed(2);
+console.log(fixdnumber);
+
+// practice problem 3
+
+var sentence1='i am goning to be';
+var sentence2= sentence1+ 'an awesome web developer';
+console.log(sentence2);
+
+// practice problem 4
+
+var num=119;
+var num1=5;
+var dividedNum=num%num1;
+console.log(dividedNum);

d93d1196220e48ffb00b3caaf53f6d7d90f1dac6
123tareq <112304893+123tareq@users.noreply.github.com>
2022-11-29 09:31:00 +0600

    Add files via upload

---
+++ arry2.js
@@ -0,0 +1,73 @@
+// what is arry
+// 1 An array is a group of related data elements kept in close proximity to one another in memory.
+// 2 The sole way to retrieve each data element directly is by using its index number, making it the most basic data structure.
+// 3  Multiple data elements of the same kind are represented by arrays under a single name.
+
+// 4 Using the index number makes it simple to access or search for a specific element within an array.
+
+
+// 5 By simply increasing the index by  1, it is simple to go across an array.
+
+//6 For all of their data elements, arrays allot memory in contiguous memory regions.
+
+// 7 The array name, the elements, and the element's data type will always be three components shared by all initializations.
+
+var celebrities =['Johnny Depp',' Arnold Schwarzenegger','Jim Carrey','Chris Evans',' Daniel Radcliffe','Robert Downey Jr',' Morgan Freeman','Tom Hanks'];
+
+console.log(celebrities.indexOf('Tom Hanks'))
+
+// first index chack
+var cars = ["Saab", "Volvo", "BMW"];
+
+var indexNumber= cars[0];
+console.log(indexNumber);
+
+
+// add and remove
+var animal=['tiger','bear','kangaru','ant','mankey','snack'];
+animal[5]='Lion';
+console.log(animal);
+
+
+// add and romove last element
+var food= ['mango','Orange','Banana','Apple','Lamon'];
+food.push('cucamber');
+food.pop();
+console.log(food);
+
+// add and remove first element 
+var array = ["a", "b", "a", "c", "a", "d"];
+array.shift();
+array.unshift("a");
+console.log(array);
+
+// about arry index;
+// 1 The array is sequential data structure in which the element can be access through the index or the element can be stored in particular location.
+// 2 The array index is start from zero.
+// 3 we also can store various typeof data like string,number.
+// 4 Arry index are more dynamic .
+// 5 if we want to castomize arry element ,we can do it to call arry index vary easyly;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

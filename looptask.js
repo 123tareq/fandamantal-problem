f3d440981d9e28a157898b61bfe99a0bb4e95370
123tareq <112304893+123tareq@users.noreply.github.com>
2022-12-01 12:31:22 +0600

    Add files via upload

---
+++ looptask.js
@@ -0,0 +1,73 @@
+
+
+// problem 1
+for(var a=0;a<=30;a++){
+    console.log("Ajka amar mon vlo nai");
+}
+
+// problem 2
+for(var number=58; number<=98; number++){
+    console.log(number);
+}
+
+ 
+// probelm 3
+for (p=240; p<=380;p++){
+    if(p%2==0){
+        console.log(p);
+    }
+}
+
+
+// probleom 4
+for(oddnuber=100; oddnuber<=820; oddnuber++){
+    if(oddnuber%2!=0){
+        console.log(oddnuber);
+    }
+}
+
+
+// porblem 4
+//  Major defferince for and while loop is , if we have starting and endig valu than we will use for loop, on the other hand if we have only conditon then while loop will be the best choice.
+
+// problem 5
+
+var arry=['a','b','c','d','e','f','g']
+var latter=0;
+while(latter<=arry.length){
+    console.log(arry);
+    latter++;
+
+}
+
+
+// problem 6
+
+var cars=['Tasla','Nican','Bmw','Rols royls','Farari'];
+for(var a=0; a<=cars.length; a++){
+    console.log(cars);
+}
+
+// problem 7
+var mobile=['nokia 1100','semphony a 15','samsung j1','Lava','realme c2','Relme 8 pro'];
+for (var i=0; i<=mobile.length; i++){
+    console.log(mobile);
+}
+
+// // problem  8
+
+// var booksPrice=[120,140,170,195,210,240,250,260,280,310];
+
+
+
+
+
+
+  
+
+
+ 
+
+
+
+

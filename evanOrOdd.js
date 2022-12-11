e642eb2b56a0ce64b9d3ba8ca8e80bb766f4a2d0
123tareq <112304893+123tareq@users.noreply.github.com>
2022-12-11 17:29:59 +0600

    Add files via upload

---
+++ evanOrOdd.js
@@ -0,0 +1,9 @@
+function oddOrEven(){
+    const a=Number(document.getElementById("inputNum").value);
+  if(a%2== 0){
+    document.write("Even Number");
+   
+  }  else{
+    document.write("Odd Number");
+  }
+}
